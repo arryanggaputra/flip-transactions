3cfd74380bdc09a7a93796ec65b3fdedf762990d
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2021-09-27 07:10:45 +0700

    :label: Add transaction entity type

---
+++ src/types/index.ts
@@ -0,0 +1,19 @@
+export interface Transaction_Entity {
+  id: string;
+  amount: number;
+  unique_code: number;
+  status: Transaction_Status_Entity;
+  sender_bank: string;
+  account_number: string;
+  beneficiary_name: string;
+  beneficiary_bank: string;
+  remark: string;
+  created_at: string;
+  completed_at: string;
+  fee: number;
+}
+
+export enum Transaction_Status_Entity {
+  SUCCESS = "SUCCESS",
+  PENDING = "PENDING",
+}
