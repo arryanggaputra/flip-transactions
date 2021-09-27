29f1690375ab8a8d09ead0f347365f558f6e21e0
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2021-09-27 07:32:19 +0700

    :lipstick: add theme to define color and spacing

---
+++ src/lib/Theme/index.ts
@@ -0,0 +1,17 @@
+const theme = {
+  colors: {
+    white: "#fff",
+    lightGray: "#f2f2f2",
+    orange: "#f3693e",
+    green: "#50b784",
+  },
+  size: {
+    xs: 4,
+    sm: 6,
+    md: 8,
+    lg: 12,
+    xl: 24,
+  },
+};
+
+export default theme;
