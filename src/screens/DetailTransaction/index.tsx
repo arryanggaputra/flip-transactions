9a0742ee67583f8e7265b2d5057bfcf51372b3cb
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2021-09-28 09:27:23 +0700

    :construction: add detail transaction page

--- src/screens/DetailTransaction/index.tsx
+++ src/screens/DetailTransaction/index.tsx
@@ -1,11 +1,80 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { RouteProp, useNavigation } from "@react-navigation/core";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import Text from "components/utils/Text";
+import View from "components/utils/View";
+import { RootStackParamList, RoutingName } from "lib/Navigation/type";
+import theme from "lib/Theme";
+import { TouchableOpacity } from "react-native";
+import formatCurrency from "lib/utils/formatCurrency";
+import formatDate from "lib/utils/formatDate";
 
-const DetailTransaction = () => {
+type TDetailTransaction = NativeStackNavigationProp<
+  RootStackParamList,
+  RoutingName.DETAIL_TRANSACTION
+>;
+
+const DetailTransaction: React.FC<{
+  route: RouteProp<RootStackParamList, RoutingName.DETAIL_TRANSACTION>;
+}> = (props) => {
+  const navigation = useNavigation<TDetailTransaction>();
+  const { data } = props.route.params;
+
   return (
-    <View>
-      <Text>Detail Transaction</Text>
-    </View>
+    <>
+      <View marginTop={theme.size.xl} backgroundColor={theme.colors.white}>
+        <View
+          padding={theme.size.xl}
+          borderBottomWidth={1}
+          borderBottomColor={theme.colors.lightGray}
+        >
+          <Text fontWeight="bold">ID TRANSAKSI: #{data.id}</Text>
+        </View>
+        <View
+          justifyContent="space-between"
+          display="flex"
+          flexDirection="row"
+          alignItems="center"
+          padding={theme.size.xl}
+          borderBottomWidth={1}
+          borderBottomColor={theme.colors.lightGray}
+        >
+          <Text fontWeight="bold">DETAIL TRANSAKSI</Text>
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Text color={theme.colors.orange}>Tutup</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View padding={theme.size.xl}>
+          <View flexDirection="row" marginBottom={theme.size.xl}>
+            <View flex={2}>
+              <Text fontWeight="bold">{data.beneficiary_name}</Text>
+              <Text>{data.account_number}</Text>
+            </View>
+            <View flex={1}>
+              <Text fontWeight="bold">NOMINAL</Text>
+              <Text>{formatCurrency(data.amount)}</Text>
+            </View>
+          </View>
+          <View flexDirection="row" marginBottom={theme.size.xl}>
+            <View flex={2}>
+              <Text fontWeight="bold">Berita Transfer</Text>
+              <Text>{data.remark}</Text>
+            </View>
+            <View flex={1}>
+              <Text fontWeight="bold">KODE UNIK</Text>
+              <Text>{data.unique_code}</Text>
+            </View>
+          </View>
+          <View flexDirection="row">
+            <View flex={2}>
+              <Text fontWeight="bold">Waktu Dibuat</Text>
+              <Text>{formatDate(data.created_at)}</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    </>
   );
 };
 
